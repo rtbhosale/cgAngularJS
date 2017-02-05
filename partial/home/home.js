53197c1bcb7a8de459a892b4a662ee454c64867b
Rahul Bhosale <rahul.b@hirecraft.in>
2017-02-05 20:21:38 +0530

    added testimonial in home page

--- partial/home/home.js
+++ partial/home/home.js
@@ -7,4 +7,14 @@ angular.module('n').controller('HomeCtrl',function($scope,$state,$rootScope,$htt
 	$http.get("https://baconipsum.com/api/?type=meat-and-filler&paras=6&format=html").then(function(response){
 		$scope.bodyContent = $sce.trustAsHtml(response.data);
 	});
+
+	$http.get("https://randomuser.me/api/?results=5").then(function(response){
+		$scope.testimonialUsers = response.data;
+	});
+
+	$http.get("https://baconipsum.com/api/?type=all-meat&sentences=5&start-with-lorem=1").then(function(response){
+		$scope.testimonialContent = response.data;
+		$scope.testimonialContent = $scope.testimonialContent[0].split(".").map(s => s.trim());
+	});
+
 });
